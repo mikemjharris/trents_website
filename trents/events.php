d127e42d042e59a33c2a597a9251c7afb468a17d
Mike Harris <mike.mj.harris@gmail.com>
2015-09-03 12:23:00 +0100

    events whitespace

--- trents/events.php
+++ trents/events.php
@@ -1,32 +1,22 @@
-
 <?php include('./header.php'); ?>
 
-<div id="gallery">
-      
-  <img alt="picture of trents" id="private_dining" src="./pics/trents_inside2.jpg" >
-  <img alt="picture of trents" id="private_dining" src="./pics/tea_trents.jpg" >
-  <img alt="picture of trents" id="private_dining" src="./pics/trents_inside.jpg" >
-  <img alt="picture of trents" src="./pics/trentsb&w.jpg" >
-
-  
-  <img alt="picture of trents" src="./pics/food10.jpg" >
-  
-  <img alt="picture of trents" src="./pics/food12.jpg" >
-  <img alt="picture of trents" src="./pics/food13.jpg" >
-  <img alt="picture of trents" src="./pics/food14.jpg" >
-  <img alt="picture of trents" src="./pics/food15.jpg" >
-  <img alt="picture of trents" src="./pics/food16.jpg" >
-  <img alt="picture of trents" src="./pics/food17.jpg" >
-  <img alt="picture of trents" src="./pics/food18.jpg" >
-  
-
-
+    <div id="gallery">
+      <img alt="picture of trents" id="private_dining" src="./pics/trents_inside2.jpg" >
+      <img alt="picture of trents" id="private_dining" src="./pics/tea_trents.jpg" >
+      <img alt="picture of trents" id="private_dining" src="./pics/trents_inside.jpg" >
+      <img alt="picture of trents" src="./pics/trentsb&w.jpg" >
+      <img alt="picture of trents" src="./pics/food10.jpg" >
+      <img alt="picture of trents" src="./pics/food12.jpg" >
+      <img alt="picture of trents" src="./pics/food13.jpg" >
+      <img alt="picture of trents" src="./pics/food14.jpg" >
+      <img alt="picture of trents" src="./pics/food15.jpg" >
+      <img alt="picture of trents" src="./pics/food16.jpg" >
+      <img alt="picture of trents" src="./pics/food17.jpg" >
+      <img alt="picture of trents" src="./pics/food18.jpg" >
       <img alt="picture of trents" src="./pics/tretns_street.jpg" >
       <img alt="picture of trents" src="./pics/black and white 8.jpg" >
       <img alt="picture of trents" src="./pics/black and white 9.jpg" >
-      
+    </div>
 
-</div>
-
-</body>
+  </body>
 </html>
