8e9b490019b62fcf235f013875d837dd45af44d7
Mike Harris <mike.mj.harris@gmail.com>
2015-09-06 22:00:11 +0100

    who we are page

---
+++ trents/who-we-are.php
@@ -0,0 +1,27 @@
+  <?php include('./header.php'); ?>
+  <section>
+    <h2>Who we are</h2>
+    <p>
+      TR3NTS takes its name from its owners.
+    </p>
+
+    <p>
+      With time, this family of three has become a household name for the Leather Lane community.
+    </p>
+
+    <p>
+      After spending over a decade opening successful restaurants for other companies, Aurora and Arthur took the plunge and opened TR3NTS in the heart of Leather Lanes 400 year old Market.
+    </p>
+
+    <p>It was February 2012.</p>
+
+    <p>
+      Before the days of TR3NTS, they often spend days walking around the narrow streets leading to Leather Lane Market and found a lack of nice places where you can sit back, relax and enjoy your surroundings without feeling pressurized to spend a fortune or scan each others dress code.
+    </p>
+
+    <p>
+      With this thought in mind TR3NTS was born. A laid back spot where you can enjoy your morning coffee, breakfast or lunch.
+    </p>
+  </section>
+  </body>
+</html>
