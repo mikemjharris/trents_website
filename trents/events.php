05517f45597cea1fc9c9d39c8cc74a029b665516
Mike Harris <mike.mj.harris@gmail.com>
2015-09-06 22:27:30 +0100

    update events page

--- trents/events.php
+++ trents/events.php
@@ -1,22 +1,24 @@
 <?php include('./header.php'); ?>
 
-    <div id="gallery">
-      <img alt="picture of trents" id="private_dining" src="./pics/trents_inside2.jpg" >
-      <img alt="picture of trents" id="private_dining" src="./pics/tea_trents.jpg" >
-      <img alt="picture of trents" id="private_dining" src="./pics/trents_inside.jpg" >
-      <img alt="picture of trents" src="./pics/trentsb&w.jpg" >
-      <img alt="picture of trents" src="./pics/food10.jpg" >
-      <img alt="picture of trents" src="./pics/food12.jpg" >
-      <img alt="picture of trents" src="./pics/food13.jpg" >
-      <img alt="picture of trents" src="./pics/food14.jpg" >
-      <img alt="picture of trents" src="./pics/food15.jpg" >
-      <img alt="picture of trents" src="./pics/food16.jpg" >
-      <img alt="picture of trents" src="./pics/food17.jpg" >
-      <img alt="picture of trents" src="./pics/food18.jpg" >
-      <img alt="picture of trents" src="./pics/tretns_street.jpg" >
-      <img alt="picture of trents" src="./pics/black and white 8.jpg" >
-      <img alt="picture of trents" src="./pics/black and white 9.jpg" >
-    </div>
+  <section>
+    <h2>Events</h2>
+      <p>
+        Since our early days, we have catered as well as delivered for various companies of different sizes.
+      </p>
+
+      <p>
+        It’s where Aurora leads and shines best.
+      </p>
+      <p>
+        From planning ahead & organizing, people delegating & product tasting, to delivery time - adequate transport, all orchestrated by Lady of the House.
+      </p>
+      <p>
+        Whether it’s breakfast for small team of 15 or a delivery for 150.
+      </p>
+      <p>
+        After all it’s what we do, so please look up our special menu for your board meetings and events.
+      </p>
+  </section>
 
   </body>
 </html>
