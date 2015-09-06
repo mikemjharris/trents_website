8639f5b30cf477979b74baff17f814cb73a105c1
Mike Harris <mike.mj.harris@gmail.com>
2015-09-06 22:33:32 +0100

    remove old code

--- trents/index.php
+++ trents/index.php
@@ -3,101 +3,12 @@
 <?php include('./header.php'); ?>
 
   <img alt="picture of trents" id="private_dining" src="./pics/private_dining2.jpg" >
-<section>
 
-   <div class="sec_pics">
-
-
-    <div id="scroll_pics">
-
-      <!-- <img alt="picture of trents" src="./pics/trentsb&w.jpg" >
-
-
-      <img alt="picture of trents" src="./pics/tretns_street.jpg" >
-      <img alt="picture of trents" src="./pics/black and white 8.jpg" >
-      <img alt="picture of trents" src="./pics/black and white 9.jpg" >
-       -->
-
-
-
-      <!-- <img alt="picture of trents" src="./pics/black and wite 7.jpg" > -->
-
-    </div>
-  </div>
-      <!-- <img alt="picture of trents" src="./pics/food7_halloumi.jpg" >
-      <img alt="picture of trents" src="./pics/food2.jpg" >
-      <img alt="picture of trents" src="./pics/trents_tea.jpg" >
-      <img alt="picture of trents" src="./pics/food8_salad.jpg" >
-      <img alt="picture of trents" src="./pics/food1.jpg" >
-
-
-      <img alt="picture of trents" src="./pics/food3.jpg" >
-
-       <img alt="picture of trents" src="./pics/food4.jpg" >
-      <img alt="picture of trents" src="./pics/food5.jpg" >
-      <img alt="picture of trents" src="./pics/food6.jpg" >
-       -->
-
-
-
-
- <!--
-
-
-
-  <div class="reservation">
-
-    Make a reservation <br />
-    <label>Location</label>
-    <select>
-      <option value="leather lane" />Leather Lane</option>
-      <option value="leather lane" />Coming Soon</option>
-    <select>
-      <label>Time</label>
-      <input type="time" min="09:00">
-
-
-      <label>Date</label>
-     <input type="date" name="date">
-     <label>Seat</label>
-     <input type="number" name="date" min="0">
-     <input type="submit">
-
-  </div> -->
-  <!-- <a href="http://www.opentable.co.uk/single.aspx?rid=103437&rtype=ism&restref=103437" class="OT_ExtLink">TR3NTS (103437), London Reservations</a> -->
 
   <a href="#" class="news">
     Receive News and Offers
   </a>
   <div class="content">
-
-    <!-- <h2>Trents - Leather Lane</h2>
-    <p>We are a family run business established in Leather Lane Market  since 2012.  Our Musetti Coffee, Homemade Cakes and wide selection of delicious Breakfast, Healthy Salads, Wraps  has earned us a well known reputation within the EC1 area. We are passionate about food and drink and offer a cosy friendly atmosphere where our customers come to enjoy our unique ambiance. </p> -->
-    <!-- <p>
-    <span class="emph">Cuisine:</span> Contemporary Global<br />
-    <span class="emph">Dining Style:</span> Casual Dining<br />
-    <span class="emph">Neighborhood:</span> London - Clerkenwell<br />
-    <span class="emph">Accepts Walk-Ins:</span> Yes<br />
-    <span class="emph">Accepts Bookings:</span>Yes<br />
-    <span class="emph">Payment: </span> All major Cards Accepted<br />
-    <span class="emph">Location:</span>  Only a short walk distance from Chancery Lane and Farringdon  Underground Station.<br />
-    <span class="emph">Parking:</span> No<br />
-    <span class="emph">Additional Details:</span> Free WiFi, Meeting Room available<br />
-
-      <span class="emph">Address:</span><br />
-        90 Leather Lane, London, EC1N 7TT<br />
-
-    Opening Hours<br />
-    Monday-Friday 8am-5pm<br />
-    Saturday 8am-3pm
-
-
-  </p> -->
-</div>
-<!-- <div id="map_canvas">
-
- <div>-->
-</section>
 
 
 
